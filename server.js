df42d8b86458653018ca707a1f13aad42d3cfbc3
JOHN_MARK_MADISON <HeavyMetalCookies@GMail.com>
2020-09-07 12:36:57 -0400

    get that page running!

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
 
 
 const http = require("http");
+const   fs = require(  "fs");
 
 function requestHandlerFunction(server_req, server_res){
-	server_res.write("hello world");
-	server_res.end();
+
+    var ucase_url = server_req.url.toUpperCase();
+
+    //:Basic routing:
+    if( ucase_url == "/" ){
+
+        fs.readFile( "./page.htm" , (err,dat)=>{
+
+            if( err ){
+                server_res.write("[ERROR_LOADING_FILE:HTM]");
+                server_res.end();
+            }else{
+                server_res.write( dat );
+                server_res.end();
+            };;
+        });;
+
+    }else
+    if( ucase_url == "/PAGE.CSS" ){
+
+        fs.readFile( "./page.css" , (err,dat)=>{
+
+            if( err ){
+                server_res.write("[ERROR_LOADING_FILE:CSS]");
+                server_res.end();
+            }else{
+                server_res.write( dat );
+                server_res.end();
+            };;
+        });;
+
+    }else{
+
+        server_res.write("[BAD_REQUEST]");
+        server_res.end();
+
+    };;
+
+
 };
 
 var server = http.createServer( requestHandlerFunction);
